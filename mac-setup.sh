c37e78334d4fbf398cbf9af71db30dd69fdb81aa
Doug Beardsley <mightybyte@gmail.com>
2016-10-04 15:50:23 -0700

    Add setup script for Mac

---
+++ mac-setup.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# macOS config steps
+#
+# whole disk encryption
+# caps lock control
+# increase keyboard repeat rate, reduce delay
+# increase resolution
+# trackpad tap to click
+# Desktop & Screen Saver - hot corners
+# Security & Privacy - Require password 5 seconds after screen saver begins
+
+ssh-keygen -b 4096 -t rsa
+
+# Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/mightybyte/dotfiles.git
+cp dotfiles/.* .
+brew install tmux
+brew install vim
+
+# Spacemacs
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+brew tap d12frosted/emacs-plus
+brew install emacs-plus
+brew linkapps emacs-plus
+
+# MenuMeters
+# http://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/zips/MenuMeters_1.9.2.zip
